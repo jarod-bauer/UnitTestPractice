047c9e2499781f97b1b7fb56abba1cfadc4a15f4
jarod-bauer <jtbauer@mail.csuchico.edu>
2022-04-27 16:36:34 -0700

    Added some test functions.

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -18,3 +18,27 @@ TEST(PracticeTest, smoke_test)
 {
     ASSERT_TRUE(true);
 }
+
+TEST(PracticeTest, palindrome_test)
+{
+		Practice pObject;
+		ASSERT_EQ(pObject.isPalindrome("racecar"), true);
+}
+
+TEST(PracticeTest, sort_test)
+{
+		Practice pObject;
+		int first = 3;
+		int second = 3;
+		int third = 5;
+		pObject.sortDescending(first, second, third);
+		ASSERT_EQ(first, 5);
+		ASSERT_EQ(second, 3);
+		ASSERT_EQ(third, 3);
+}
+
+TEST(PracticeTest, repeats_test)
+{
+		Practice pObject;
+		ASSERT_EQ(pObject.count_starting_repeats("zzzebra"), 3);
+}
